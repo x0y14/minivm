e840453f2a3f93fed3be006f486de4a503c28b16
x0y14 <ya01ma08@gmail.com>
2025-10-09 21:29:22 +0900

    parse

---
+++ compile/object/node.go
@@ -0,0 +1,299 @@
+package object
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/x0y14/minivm/compile"
+)
+
+type Node interface {
+	isNode()
+	String() string
+}
+
+type Operation int
+
+const (
+	_ Operation = iota
+	NOP
+	MOV
+	PUSH
+	POP
+	ALLOC
+	STORE
+	LOAD
+	CALL
+	RET
+	JMP
+	JZ
+	JNZ
+	ADD
+	SUB
+	EQ
+	NE
+	LT
+	LE
+	SYSCALL
+)
+
+func (o Operation) isNode() {}
+func (o Operation) String() string {
+	return []string{
+		NOP:     "nop",
+		MOV:     "mov",
+		PUSH:    "push",
+		POP:     "pop",
+		ALLOC:   "alloc",
+		STORE:   "store",
+		LOAD:    "load",
+		CALL:    "call",
+		RET:     "ret",
+		JMP:     "jmp",
+		JZ:      "jz",
+		JNZ:     "jnz",
+		ADD:     "add",
+		SUB:     "sub",
+		EQ:      "eq",
+		NE:      "ne",
+		LT:      "lt",
+		LE:      "le",
+		SYSCALL: "syscall",
+	}[o]
+}
+
+// Instruction `mov dst src`のような命令
+type Instruction struct {
+	Op   Operation
+	Args []Node
+}
+
+func (i Instruction) isNode() {}
+func (i Instruction) String() string {
+	var elms []string
+	for _, elm := range i.Args {
+		elms = append(elms, elm.String())
+	}
+	return fmt.Sprintf("Instruction{ Op: %s, Args: [ %s ] }", i.Op.String(), strings.Join(elms, ", "))
+}
+
+// Register `[sp+1]`の `sp`部分
+type Register int
+
+const (
+	_ Register = iota
+	PC
+	SP
+	BP
+	HP
+
+	R0
+	R1
+	R2
+	R3
+	R4
+	R5
+	R6
+	R7
+	R8
+	R9
+	R10
+
+	ZF
+)
+
+func (r Register) isNode() {}
+func (r Register) String() string {
+	return []string{
+		PC:  "pc",
+		SP:  "sp",
+		BP:  "bp",
+		HP:  "hp",
+		R0:  "r0",
+		R1:  "r1",
+		R2:  "r2",
+		R3:  "r3",
+		R4:  "r4",
+		R5:  "r5",
+		R6:  "r6",
+		R7:  "r7",
+		R8:  "r8",
+		R9:  "r9",
+		R10: "r10",
+		ZF:  "zf",
+	}[r]
+}
+
+// Offset `[sp+1]`のような相対位置
+type Offset struct {
+	Target Register
+	Diff   int
+}
+
+func (o Offset) isNode() {}
+func (o Offset) String() string {
+	return fmt.Sprintf("[%s%+d]", o.Target.String(), o.Diff)
+}
+
+type Number int
+
+func (n Number) isNode() {}
+func (n Number) String() string {
+	return strconv.Itoa(int(n))
+}
+
+type Character rune
+
+func (c Character) isNode() {}
+func (c Character) String() string {
+	return strconv.QuoteRune(rune(c))
+}
+
+type Label struct {
+	Define bool
+	Name   string
+}
+
+func (l Label) isNode() {}
+func (l Label) String() string {
+	return l.Name
+}
+
+var curt *compile.Token
+
+func expect(kind compile.TokenKind) (*compile.Token, error) {
+	if curt.Kind != kind {
+		return nil, fmt.Errorf("want=%s, got=%s", kind.String(), curt.Kind.String())
+	}
+	v := *curt
+	curt = curt.Next
+	return &v, nil
+}
+func consume(kind compile.TokenKind) *compile.Token {
+	if curt.Kind != kind {
+		return nil
+	}
+	v := *curt
+	curt = curt.Next
+	return &v
+}
+
+func parseStackOffset() ([]Node, error) {
+	// [
+	if _, err := expect(compile.Lcb); err != nil {
+		return nil, err
+	}
+
+	// sp / bp
+	id, err := expect(compile.Identifier)
+	if err != nil {
+		return nil, err
+	}
+	var reg Register
+	switch string(id.Raw) {
+	case "sp":
+		reg = SP
+	case "bp":
+		reg = BP
+	default:
+		return nil, fmt.Errorf("unsupported register: %s", string(id.Raw))
+	}
+
+	// +
+	plus := consume(compile.Add)
+	// -
+	minus := consume(compile.Sub)
+	if plus != nil && minus != nil {
+		return nil, fmt.Errorf("syntax err")
+	}
+
+	// diff
+	diff, err := expect(compile.Integer)
+	if err != nil {
+		return nil, err
+	}
+
+	// ]
+	if _, err := expect(compile.Rcb); err != nil {
+		return nil, err
+	}
+
+	v, err := diff.GetValueAsInteger()
+	if err != nil {
+		return nil, err
+	}
+	if minus != nil {
+		return []Node{Offset{reg, -v}}, nil
+	}
+	return []Node{Offset{reg, v}}, nil
+}
+
+func parseLabel() ([]Node, error) {
+	// id
+	id, err := expect(compile.Identifier)
+	if err != nil {
+		return nil, err
+	}
+
+	// :
+	var define = false
+	if t := consume(compile.Colon); t != nil {
+		define = true
+	}
+
+	return []Node{Label{define, string(id.Raw)}}, nil
+}
+
+func Parse(token *compile.Token) ([]Node, error) {
+	var nodes []Node
+	curt = token
+loop:
+	for {
+		switch curt.Kind {
+		case compile.Eof:
+			break loop
+		case compile.Comment:
+			curt = curt.Next
+		case compile.Identifier:
+			if op, yes := isOperation(string(curt.Raw)); yes {
+				nodes = append(nodes, op)
+				curt = curt.Next
+				continue
+			}
+			if reg, yes := isRegister(string(curt.Raw)); yes {
+				nodes = append(nodes, reg)
+				curt = curt.Next
+				continue
+			}
+			nds, err := parseLabel()
+			if err != nil {
+				return nil, err
+			}
+			nodes = append(nodes, nds...)
+		case compile.Integer:
+			v, err := curt.GetValueAsInteger()
+			if err != nil {
+				return nil, err
+			}
+			nodes = append(nodes, Number(v))
+			curt = curt.Next
+		case compile.Char:
+			v, err := curt.GetValueAsRune()
+			if err != nil {
+				return nil, err
+			}
+			nodes = append(nodes, Character(v))
+			curt = curt.Next
+		case compile.Lcb:
+			nds, err := parseStackOffset()
+			if err != nil {
+				return nil, err
+			}
+			nodes = append(nodes, nds...)
+		default:
+			return nil, fmt.Errorf("parse: unsupported token: %s", curt.Kind.String())
+		}
+	}
+	return nodes, nil
+}
---
+++ compile/object/node_test.go
@@ -0,0 +1,121 @@
+package object
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/x0y14/minivm/compile"
+)
+
+func TestParse_Basic(t *testing.T) {
+	input := `
+mov r0 1
+add r1 r0
+push 'a'
+`
+	toks, err := compile.Tokenize([]rune(input))
+	if err != nil {
+		t.Fatalf("tokenize error: %v", err)
+	}
+	nodes, err := Parse(toks)
+	if err != nil {
+		t.Fatalf("parse error: %v", err)
+	}
+	expect := []Node{
+		MOV, R0, Number(1),
+		ADD, R1, R0,
+		PUSH, Character('a'),
+	}
+	if diff := cmp.Diff(expect, nodes); diff != "" {
+		t.Errorf("diff:\n%s", diff)
+	}
+}
+
+func TestParse_Offsets(t *testing.T) {
+	tests := []struct {
+		name   string
+		input  string
+		expect []Node
+	}{
+		{
+			name:  "pc offset +",
+			input: "_label:",
+			expect: []Node{
+				Label{true, "_label"},
+			},
+		},
+		{
+			name:  "pc offset -",
+			input: "jmp _label",
+			expect: []Node{
+				JMP, Label{false, "_label"},
+			},
+		},
+		{
+			name:  "stack offset sp +",
+			input: "load r2 [sp+4]",
+			expect: []Node{
+				LOAD, R2, Offset{Target: SP, Diff: 4},
+			},
+		},
+		{
+			name:  "stack offset bp -",
+			input: "store [bp-2] r3",
+			expect: []Node{
+				STORE, Offset{Target: BP, Diff: -2}, R3,
+			},
+		},
+		{
+			name:  "char",
+			input: "'r'1",
+			expect: []Node{
+				Character('r'), Number(1),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			toks, err := compile.Tokenize([]rune(tt.input))
+			if err != nil {
+				t.Fatalf("tokenize error: %v", err)
+			}
+			nodes, err := Parse(toks)
+			if err != nil {
+				t.Fatalf("parse error: %v", err)
+			}
+			if diff := cmp.Diff(tt.expect, nodes); diff != "" {
+				t.Errorf("diff:\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestParse_SkipComment(t *testing.T) {
+	input := "mov r0 1 ; comment\nadd r0 2"
+	toks, err := compile.Tokenize([]rune(input))
+	if err != nil {
+		t.Fatalf("tokenize error: %v", err)
+	}
+	nodes, err := Parse(toks)
+	if err != nil {
+		t.Fatalf("parse error: %v", err)
+	}
+	expect := []Node{
+		MOV, R0, Number(1),
+		ADD, R0, Number(2),
+	}
+	if diff := cmp.Diff(expect, nodes); diff != "" {
+		t.Errorf("diff:\n%s", diff)
+	}
+}
+
+func TestParse_Error_StackOffset_UnsupportedRegister(t *testing.T) {
+	input := "[hp+1]"
+	toks, err := compile.Tokenize([]rune(input))
+	if err != nil {
+		t.Fatalf("tokenize error: %v", err)
+	}
+	if _, err := Parse(toks); err == nil {
+		t.Fatalf("expected error, got nil")
+	}
+}
